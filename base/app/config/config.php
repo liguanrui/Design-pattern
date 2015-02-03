173c63e5e9852bd6968cae65d7b74f78927f1748
liguanrui <xhsysu@126.com>
2015-02-04 00:08:21 +0800

    【开发】增加自动加载类

--- base/app/config/config.php
+++ base/app/config/config.php
@@ -1,12 +1,22 @@
 <?php 
+namespace app\config;
 /**
 *	@label 定义各个目录常量
 */
 define ( 'SYSDIR_ROOT', realpath ( dirname ( __FILE__ ) . '/../../' ) );
-define ( 'SYSDIR_APP', SYSDIR_ROOT.'/app' ) );
-define ( 'SYSDIR_SYSTEM', SYSDIR_ROOT.'/system' ) );
-define ( 'SYSDIR_CONTROLLER', SYSDIR_APP.'/controller' ) );
-define ( 'SYSDIR_MODAL', SYSDIR_APP.'/model' ) );
-define ( 'SYSDIR_VIEW', SYSDIR_APP.'/view' ) );
-define ( 'SYSDIR_CORE', SYSDIR_SYSTEM.'/core' ) );
-define ( 'SYSDIR_DATABASE', SYSDIR_SYSTEM.'/database' ) );
+define ( 'SYSDIR_APP', SYSDIR_ROOT.'/app');
+define ( 'SYSDIR_SYSTEM', SYSDIR_ROOT.'/system');
+define ( 'SYSDIR_CONTROLLER', SYSDIR_APP.'/controller');
+define ( 'SYSDIR_MODAL', SYSDIR_APP.'/model');
+define ( 'SYSDIR_VIEW', SYSDIR_APP.'/view');
+define ( 'SYSDIR_CORE', SYSDIR_SYSTEM.'/core');
+define ( 'SYSDIR_DATABASE', SYSDIR_SYSTEM.'/database');
+
+
+spl_autoload_register(autoload);
+
+function autoload($class){
+	$file = SYSDIR_ROOT.'/'.str_replace('\\','/',$class).'.php';
+	include $file;
+	
+}
