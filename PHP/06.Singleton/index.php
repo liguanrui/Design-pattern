886d9249e1321f94f50aa83b64392f78309aad70
liguanrui <xhsysu@126.com>
2022-03-09 20:53:33 +0800

    #feature Singleton

---
+++ PHP/06.Singleton/index.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace PHP\Singleton;
+
+function main()
+{
+    $c1 = Cache::getInstance();
+    $c2 = Cache::getInstance();
+
+
+    if ($c1 === $c2) {
+        echo "this is a single instance.";
+    } else {
+        echo "this is not single instance.";
+    }
+}
+
+class Cache
+{
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if (empty(self::$instance)) {
+            return self::$instance = new static();
+        } else {
+            return self::$instance;
+        }
+    }
+
+
+}
+
+main();
